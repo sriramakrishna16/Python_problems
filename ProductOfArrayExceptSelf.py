fb567133fdc799149fb3aadaf782422cff65bf76
sriramakrishna16 <srirambvc16@gmail.com>
2026-09-25 14:10:01 +0530

    Product of array except self code added

---
+++ ProductOfArrayExceptSelf.py
@@ -0,0 +1,15 @@
+def productOfArray(nums):
+    result = [1] * len(nums)
+    prefix = 1
+    for i in range(len(nums)):
+        result[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums)-1, -1, -1):
+        result[i] *= postfix
+        postfix *= nums[i]
+    
+    return result
+
+nums = [1,2,3,4]
+print(productOfArray(nums))
